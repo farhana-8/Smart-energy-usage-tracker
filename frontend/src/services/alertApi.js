d481953ce1a2eb0d4fc4e83a78dfedf650a5e756
FARHANATHU SAQTHIYA S <152422349+farhana-8@users.noreply.github.com>
2026-01-06 11:03:26 +0530

    Delete frontend directory

--- frontend/src/services/alertApi.js
+++
@@ -1,8 +0,0 @@
-// src/services/alertService.js
-import api from "./authService"; // reuse the existing api instance with token
-
-export const fetchEnergyAlert = async (email) => {
-  const trimmedEmail = email.trim();
-  const response = await api.get(`/alerts/${trimmedEmail}`);
-  return response.data;
-};
